69bbd072291a0b0f0233b7b2f9b592478947aa56
yaozhiming <1243204919@qq.com>
2022-04-14 11:55:33 +0800

    清理

--- Node/基础/3Node内置模块/2文件路径.js
+++
@@ -1,2 +0,0 @@
-console.log(__dirname);//目录路径
-console.log(__filename);//文件路径
--- Node/基础/4lesson-two-express-init/lesson-two-express/1npm项目工程/progrem-shop/src/index.js
+++
@@ -1,7 +0,0 @@
-
-  const express = require('expresss');
-  const app = express();
-  app.get('*',function(){
-    console.log("111");
-  })
-  app.listen(3000)
